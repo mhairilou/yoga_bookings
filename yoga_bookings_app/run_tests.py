e018f6167b3dece7037f8332db535f2c07edf651
Mhairi MacDonald <mhairilouyoga@gmail.com>
2022-08-22 13:34:41 +0100

    tested student class

--- yoga_bookings_app/run_tests.py
+++ yoga_bookings_app/run_tests.py
@@ -1,5 +1,6 @@
 import unittest
 from tests.session_test import TestSession
+from tests.student_test import TestStudent
 
 if __name__ == '__main__':
     unittest.main()
---
+++ yoga_bookings_app/tests/student_test.py
@@ -0,0 +1,22 @@
+import unittest
+
+from models.student import Student
+
+
+class TestStudent(unittest.TestCase):
+    
+    def setUp(self):
+        self.student1 = Student("Woody", "The Dog", 2)
+        
+    def test_student_has_first_name(self):
+        result = self.student1.first_name
+        self.assertEqual("Woody", result)
+
+    def test_student_has_last_name(self):
+        result = self.student1.last_name
+        self.assertEqual("The Dog", result)
+    
+    def test_student_has_credits(self):
+        result = self.student1.credits
+        self.assertEqual(2, result)    
+        
